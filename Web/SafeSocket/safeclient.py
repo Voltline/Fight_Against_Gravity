a1637838e23c5854ba687c600f6623571f31f28e
ShiinaRikka <50125419+lky1433223@users.noreply.github.com>
2022-11-07 17:23:53 +0800

    lky::server sockets实现心跳检测，实现json格式封装转换， 之后计划： client端的接受json消息 socket的json格式设计， socket的加密解密封装到包里 main操作队列

--- Web/SafeSocket/safeclient.py
+++ Web/SafeSocket/safeclient.py
@@ -1,28 +1,49 @@
-﻿import socket
+﻿import json
+import socket
+import time
+from threading import Thread
 
 
 class SocketClient:
     """
     使用TCP连接的socket函数封装
     """
-    def __init__(self, ip: str, port: int):
+
+    def __init__(self, ip: str, port: int, heart_beat: int = -1):
         """
+        ip:服务端ip地址
+        port:服务端端口
+        heart_beat:心跳检测 默认0
         初始化
         初始化后已经和服务端建立了socket连接
         """
         self.__socket = socket.socket()
         self.__port = port
         self.__host = ip
+        self.heart_beat = heart_beat
         try:
             self.__socket.connect((self.__host, self.__port))
         except Exception as err:
             print(err, "无法连接到服务器")
+        if heart_beat > 0:
+            self.heart_thread = Thread(target=self.beating)
+            self.heart_thread.setDaemon(True)
+            self.heart_thread.start()
 
-    def send(self, data):
+    def beating(self):
+        while True:
+            msg = {"opt": 0, "heartbeat": self.heart_beat}
+            self.send(msg)
+            time.sleep(self.heart_beat - 0.4)
+
+    def send(self, msg):
         """
+        data:数据 支持str/json格式
         发送数据
         """
-        self.__socket.send(str(data).encode())
+        if type(msg) == dict:
+            msg = json.dumps(msg)
+        self.__socket.sendall(msg.encode())
 
     def receive(self):
         """
@@ -40,11 +61,16 @@ class SocketClient:
 if __name__ == "__main__":
     ip = "localhost"
     port = 25555
-    client = SocketClient(ip, port)
+    client = SocketClient(ip, port, 5)
+    print(1)
     while True:
         a = input()
         if a == "0":
             break
+        a = {
+            "opt": -1,
+            "info": a
+        }
+        print(a)
         client.send(a)
-        # print(client.receive())
     client.close()
