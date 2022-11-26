38c9c31fa59b812d515ff71bf3ac3b6911aa0b16
Voltline <44248193@qq.com>
2022-11-26 21:33:58 +0800

    zyh: 更改了尾迹颜色的计算方法 * 尾迹出现时间born_ms//10，mod360再减90（保证将其映射在-90°~270°以内并均匀变化) * 转换为弧度后构造三个相位差2pi/3的cos函数取平方（保证其映射在0~1以内） * 再把所得值乘255，即可获得均匀变化的渐变色曲线

--- content/trace.py
+++ content/trace.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Vector2
 from random import randint
+import math
 
 
 class Trace:
@@ -10,9 +11,9 @@ class Trace:
     def __init__(self, settings, loc0: Vector2, loc1: Vector2, born_ms):
         self.loc0 = loc0.copy()
         self.loc1 = loc1.copy()
-        self.color = self.get_color()
         self.born_ms = born_ms  # 尾迹出现的时间戳(ms)
         self.life_ms = settings.trace_life_ms  # 尾迹持续的时间(ms)
+        self.color = self.get_color()
 
     def is_alive(self) -> bool:
         """判断尾迹是否还应该存在"""
@@ -22,10 +23,10 @@ class Trace:
         """在screen上绘制"""
         camera.draw_line(self.loc0, self.loc1, self.color)
 
-    @staticmethod
-    def get_color() -> (int, int, int):
+    #@staticmethod
+    def get_color(self) -> (int, int, int):
         """随时间改变颜色"""
-        i = randint(0, 4)
-        if i < 3:
-            Trace.__color[i] = (Trace.__color[i] + 1) % 256
+        Trace.__color[0] = int((math.cos(((self.born_ms//10 % 360 - 90)*math.pi)/360))**2 * 255)
+        Trace.__color[1] = int((math.cos(((self.born_ms//10 % 360 - 90)*math.pi)/360 - 2 * math.acos(-1)/3))**2 * 255)
+        Trace.__color[2] = int((math.cos(((self.born_ms//10 % 360 - 90)*math.pi)/360 + 2 * math.acos(-1)/3))**2 * 255)
         return Trace.__color[:]
