29df5e5104a350419e14c283ac0cb130a3ec9f95
Voltline <44248193@qq.com>
2022-12-13 13:27:12 +0800

    zyh: 修复identify_server无法正常运行的问题

--- Server/Modules/send_email.py
+++ Server/Modules/send_email.py
@@ -2,6 +2,9 @@ import smtplib
 import random
 import json
 import sys
+import os
+path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/"
+print(path)
 from email.mime.text import MIMEText
 from email.header import Header
 
@@ -25,7 +28,7 @@ def send_email(user_name: str, target_email: str, id_code: str) -> None:
     :参数：user_name：用户名，target_email：目标邮箱，id_code：验证码
     :返回：无返回
     """
-    with open("Modules/settings.json", 'r') as f:
+    with open(path + "settings/settings.json", 'r') as f:
         information = json.load(f)
     my_mail = information["Email"]["Addr"]
     password = information["Email"]["Password"]
