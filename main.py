bdcf806a9840b2061d012c6d0da3da8623d591e3
Voltline <44248193@qq.com>
2022-12-15 23:21:29 +0800

    zyh: 拆分服务器与游戏本体

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import os
 
 path = os.path.dirname(os.path.realpath(__file__)) + '/'
 sys.path.append(path)
-from Server import server_main
 from Server import client_main
-import content.game.game_function as gf
-from content.game.local_game import LocalGame
 from settings.all_settings import Settings
 
 """
@@ -22,21 +19,10 @@ u：玩家2的发射子弹u
 鼠标滚轮：视角缩放
 """
 settings = Settings(path)  # 初始化设置类
-if len(sys.argv) >= 2:
-    _debug_ = "--debug" in sys.argv
-    if sys.argv[1] == "--server":
-        os.environ['SDL_VIDEODRIVER'] = 'dummy'
-        s = server_main.ServerMain(game_settings=settings, path=path, _debug_=_debug_)
-        s.start()
-        del os.environ['SDL_VIDEODRIVER']
-    elif sys.argv[1] == "--client":
-        s = client_main.ClientMain(path, _debug_=_debug_)
-        s.start()
-        # check_code = s.register_get_checkcode("test_1", "541665621@qq.com")
-        # input_check_code = input()
-        # ps = input()
-        # s.register_push_password("test_1", "541665621@qq.com", check_code, input_check_code, ps)
-else:
-    screen = gf.init_pygame_window(settings)
-    game = LocalGame(settings, screen, '征途')
-    game.main()
+_debug_ = False
+s = client_main.ClientMain(path, _debug_=_debug_)
+s.start()
+# check_code = s.register_get_checkcode("test_1", "541665621@qq.com")
+# input_check_code = input()
+# ps = input()
+# s.register_push_password("test_1", "541665621@qq.com", check_code, input_check_code, ps)
---
+++ server.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+path = os.path.dirname(os.path.realpath(__file__)) + '/'
+sys.path.append(path)
+from Server import server_main
+from settings.all_settings import Settings
+
+settings = Settings(path)  # 初始化设置类
+_debug_ = False
+os.environ['SDL_VIDEODRIVER'] = 'dummy'
+s = server_main.ServerMain(game_settings=settings, path=path, _debug_=_debug_)
+s.start()
+del os.environ['SDL_VIDEODRIVER']
