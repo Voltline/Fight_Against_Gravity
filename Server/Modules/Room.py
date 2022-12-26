c52815ee8a2bf048321342bf44db4752377de216
ShiinaRikka <50125419+lky1433223@users.noreply.github.com>
2022-12-26 19:16:51 +0800

    lky::address不传了

--- Server/Modules/Room.py
+++ Server/Modules/Room.py
@@ -54,16 +54,12 @@ class Room:
 
     def start(self):
         self.started = True
-        user_adr = {}
-        for user in self.userlist:
-            user_adr[user.get_name()] = user.get_address()
         self.game = server_game.ServerGame(
             settings=self.game_settings,
             net=self.server_,
             room_id=self.roomid,
             map_name=self.roommap,
-            player_names=self.get_userlist(),
-            addresses=user_adr
+            player_names=self.get_userlist()
         )
         thread = threading.Thread(target=self.game.main)
         thread.setDaemon(True)
