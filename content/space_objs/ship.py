dfa6e6644bd1932b037eba0a98fee7b435738d9c
D_S_O_ <lty20021010@icloud.com>
2022-12-26 21:26:37 +0800

    lty: 调试开始游戏的功能；让本地游戏在请求开始游戏前给服务器发个请求消息，防止服务器自动发的消息被本地遗漏

--- content/space_objs/ship.py
+++ content/space_objs/ship.py
@@ -30,7 +30,6 @@ class Ship(SpaceObj):
         self.go_acc = settings.ship_go_acc  # 引擎的加速度
         self.turn_spd = settings.ship_turn_spd  # 转向的角速度
 
-        #TODO:ll
         if "--nogui" not in sys.argv:
             self.status_bar = StatusBar(settings, self.player_name)
 
@@ -101,7 +100,6 @@ class Ship(SpaceObj):
     def display(self, camera):
         if self.image is not None:
             super().display(camera)
-        #TODO:ll
         if "--nogui" not in sys.argv:
             if self.is_alive:  # 还活着就更新并显示status_bar
                 self.status_bar.update_hp(self.hp)
